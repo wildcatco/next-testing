4df0cab5b80e8ce9df8d6f13172c8ca863b2f6f9
Jihoo <53351097+wildcatco@users.noreply.github.com>
2023-01-07 14:42:54 +0900

    E2e test - static routes

---
+++ cypress/e2e/routes.cy.ts
@@ -0,0 +1,13 @@
+it("displays correct heading when navigating to shows routes", () => {
+  cy.visit("/");
+  cy.findByRole("button", { name: /shows/i }).click();
+  cy.findByRole("heading", { name: /upcoming shows/i }).should("exist");
+});
+
+it("displays correct heading when navigating to bands routes", () => {
+  cy.visit("/");
+  cy.findByRole("button", { name: /bands/i }).click();
+  cy.findByRole("heading", { name: /our illustrious performers/i }).should(
+    "exist"
+  );
+});
