b22e31580809fa37e6a92d17c847ce04fd62a82a
Jihoo <53351097+wildcatco@users.noreply.github.com>
2023-01-09 23:02:06 +0900

    Test api - POST /api/reservations/[reservationId] not authorized

--- __tests__/api/reservations.test.ts
+++ __tests__/api/reservations.test.ts
@@ -1,9 +1,11 @@
 import { testApiHandler } from "next-test-api-route-handler";
 
+import { validateToken } from "@/lib/auth/utils";
 import createReservationHandler from "@/pages/api/reservations/[reservationId]";
 import userReservationsHandler from "@/pages/api/users/[userId]/reservations";
 
 jest.mock("@/lib/auth/utils");
+const mockValidateToken = validateToken as jest.Mock;
 
 test("POST /api/reservations/[reservationId] creates a reservation", async () => {
   await testApiHandler({
@@ -41,6 +43,33 @@ test("POST /api/reservations/[reservationId] creates a reservation", async () =>
       const json = await res.json();
 
       expect(json.userReservations).toHaveLength(3);
+    },
+  });
+});
+
+test("POST /api/reservations/[reservationId] returns 401 status when not authorized", async () => {
+  mockValidateToken.mockResolvedValue(false);
+
+  await testApiHandler({
+    handler: createReservationHandler,
+    paramsPatcher: (params) => {
+      // eslint-disable-next-line no-param-reassign
+      params.reservationId = 12345;
+    },
+    test: async ({ fetch }) => {
+      const res = await fetch({
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          seatCount: 5,
+          userId: 1,
+          showId: 0,
+        }),
+      });
+
+      expect(res.status).toBe(401);
     },
   });
 });
