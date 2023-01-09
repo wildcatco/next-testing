d50b2060fec4f2e2f8900990d74197a59bcda02d
Jihoo <53351097+wildcatco@users.noreply.github.com>
2023-01-09 14:01:39 +0900

    E2e test - login succeeds

---
+++ cypress/e2e/auth-wrapper.cy.ts
@@ -0,0 +1,36 @@
+it("runs auth flow for successful login to protected reservations page", () => {
+  // visit reservations page for the first show (id = 0)
+  cy.task("db:reset").visit("/reservations/0");
+
+  // check for sign in form
+  cy.findByRole("heading", { name: /sign in to your account/i }).should(
+    "exist"
+  );
+
+  // check that there's no option to purchase tickets
+  cy.findByRole("button", { name: /purchase/i }).should("not.exist");
+
+  // enter valid sign-in credentials
+  cy.findByLabelText(/email address/i)
+    .clear()
+    .type(Cypress.env("USER_EMAIL"));
+  cy.findByLabelText(/password/i)
+    .clear()
+    .type(Cypress.env("PASSWORD"));
+
+  // submit the form
+  cy.findByRole("main").within(() => {
+    cy.findByRole("button", { name: /sign in/i }).click();
+  });
+
+  // check for purchase button and band name
+  cy.findByRole("button", { name: /purchase/i }).should("exist");
+  cy.findByRole("heading", { name: /the wandering bunnies/i }).should("exist");
+
+  // check for email and sign-out button on navbar
+  cy.findByRole("button", { name: Cypress.env("USER_EMAIL") }).should("exist");
+  cy.findByRole("button", { name: /sign out/i }).should("exist");
+
+  // check that sign in button does not exists
+  cy.findByRole("button", { name: /sign in/i }).should("not.exist");
+});
