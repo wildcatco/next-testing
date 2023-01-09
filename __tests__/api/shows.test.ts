7e73b5ac67ad40116121ca8be6245c10e09879d8
Jihoo <53351097+wildcatco@users.noreply.github.com>
2023-01-09 20:42:43 +0900

    Test GET /api/shows/[showId]

--- __tests__/api/shows.test.ts
+++ __tests__/api/shows.test.ts
@@ -1,6 +1,7 @@
 import { testApiHandler } from "next-test-api-route-handler";
 
 import { readFakeData } from "@/__tests__/__mocks__/fakeData";
+import showIdHandler from "@/pages/api/shows/[showId]";
 import showsHandler from "@/pages/api/shows/index";
 
 test("GET /api/shows returns shows from database", async () => {
@@ -13,6 +14,24 @@ test("GET /api/shows returns shows from database", async () => {
 
       const { fakeShows } = await readFakeData();
       expect(json).toEqual({ shows: fakeShows });
+    },
+  });
+});
+
+test("GET /api/shows/[showId] returns the data for the correct show ID", async () => {
+  await testApiHandler({
+    handler: showIdHandler,
+    paramsPatcher: (params) => {
+      // eslint-disable-next-line no-param-reassign
+      params.showId = 0;
+    },
+    test: async ({ fetch }) => {
+      const res = await fetch({ method: "GET" });
+      expect(res.status).toBe(200);
+      const json = await res.json();
+
+      const { fakeShows } = await readFakeData();
+      expect(json).toEqual({ show: fakeShows[0] });
     },
   });
 });
