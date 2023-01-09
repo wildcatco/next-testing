001efee41db6246cb6fff98a7b9670bb446c9feb
Jihoo <53351097+wildcatco@users.noreply.github.com>
2023-01-09 23:07:16 +0900

    Test api - GET /api/users/[userId]/reservations unauthorized

--- __tests__/api/user.test.ts
+++ __tests__/api/user.test.ts
@@ -1,9 +1,11 @@
 import { testApiHandler } from "next-test-api-route-handler";
 
+import { validateToken } from "@/lib/auth/utils";
 import userReservationsHandler from "@/pages/api/users/[userId]/reservations";
 import userAuthHandler from "@/pages/api/users/index";
 
 jest.mock("@/lib/auth/utils");
+const mockValidateToken = validateToken as jest.Mock;
 
 test("POST /api/users receives token with correct credentials", async () => {
   await testApiHandler({
@@ -61,6 +63,22 @@ test("GET /api/users/[userId]/reservations returns empty array for user without 
       const json = await res.json();
 
       expect(json.userReservations).toHaveLength(0);
+    },
+  });
+});
+
+test("GET /api/users/[userId]/reservations returns 401 status when not authorized", async () => {
+  mockValidateToken.mockResolvedValue(false);
+
+  await testApiHandler({
+    handler: userReservationsHandler,
+    paramsPatcher: (params) => {
+      // eslint-disable-next-line no-param-reassign
+      params.userId = 1;
+    },
+    test: async ({ fetch }) => {
+      const res = await fetch({ method: "GET" });
+      expect(res.status).toBe(401);
     },
   });
 });
