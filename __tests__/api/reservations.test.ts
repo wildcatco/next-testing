7c94ee56db5cef3a8f65013f37cf7f0879282ebf
Jihoo <53351097+wildcatco@users.noreply.github.com>
2023-01-09 21:37:14 +0900

    Test api - POST /api/reservations/[reservationId]

---
+++ __tests__/api/reservations.test.ts
@@ -0,0 +1,46 @@
+import { testApiHandler } from "next-test-api-route-handler";
+
+import createReservationHandler from "@/pages/api/reservations/[reservationId]";
+import userReservationsHandler from "@/pages/api/users/[userId]/reservations";
+
+jest.mock("@/lib/auth/utils");
+
+test("POST /api/reservations/[reservationId] creates a reservation", async () => {
+  await testApiHandler({
+    handler: createReservationHandler,
+    paramsPatcher: (params) => {
+      // eslint-disable-next-line no-param-reassign
+      params.reservationId = 12345;
+    },
+    test: async ({ fetch }) => {
+      const res = await fetch({
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          seatCount: 5,
+          userId: 1,
+          showId: 0,
+        }),
+      });
+
+      expect(res.status).toBe(201);
+    },
+  });
+
+  await testApiHandler({
+    handler: userReservationsHandler,
+    paramsPatcher: (params) => {
+      // eslint-disable-next-line no-param-reassign
+      params.userId = 1;
+    },
+    test: async ({ fetch }) => {
+      const res = await fetch({ method: "GET" });
+      expect(res.status).toBe(200);
+      const json = await res.json();
+
+      expect(json.userReservations).toHaveLength(3);
+    },
+  });
+});
